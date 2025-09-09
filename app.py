eeff38598b1ba734b942a55db646bd3b6aa5867b
Archanapixel <23jr1a0588@gmail.com>
2025-09-09 15:00:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import flask
+from model import generatorAI
+import pickle
+
+generateAI()
+ai=pickle.load(open('ai.pkl','rb'))
+app=Flask(__name__)
+
+
+@app.route("/")
+ def homepage():
+    return"Server Running"
+
+
+@app.route("/predict")
+def predict():
+    ir=request.args.get('ir')
+    ir=int(ir)
+    data=[[ir]]
+    result=ai.predict(data)
+    return result
+if(__name__)=="__main.py__":
+    app.run(host='0.0.0.0',port=3000)
